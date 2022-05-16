cbd9a66143098c455356c588d9a6896adebc5613
wertyu782001 <103937039+wertyu782001@users.noreply.github.com>
2022-05-16 21:34:51 +0800

    彈珠軌跡與程式資料

--- Assets/DATA/Learndata.cs
+++ Assets/DATA/Learndata.cs
@@ -1,6 +1,13 @@
+//遊戲UNITY設計20220516 阿翔(林毅翔)
 using UnityEngine;
 
 public class Learndata : MonoBehaviour
 {
-   
+    int hp;
+
+    int lv;
+    float exp;
+    string PlayerName;
+    bool hpCureskill;
+
 }
