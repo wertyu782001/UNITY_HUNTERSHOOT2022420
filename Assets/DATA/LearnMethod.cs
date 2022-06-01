a80fcece23d97e3b4612561d101ed7e17a9213f4
wertyu782001 <103937039+wertyu782001@users.noreply.github.com>
2022-06-01 21:02:03 +0800

    鏡頭特效與程式語法

--- Assets/DATA/LearnMethod.cs
+++ Assets/DATA/LearnMethod.cs
@@ -2,10 +2,23 @@ using UnityEngine;
 
 public class LearnMethod : MonoBehaviour
 {
-    void Test()
+    private void Test()
     {
         print("哈囉，沃德");
     }
+    private void PrintColorText()
+    {
+        print("<color=#96FED1>顏色訊息</color>");
+        print("<color=#FFC1E0>顏色訊息</color>");
+        print("<color=#7D7DFF>顏色訊息</color>");
 
- 
+    }        
+
+    private void Start()
+    {
+        Test();
+        PrintColorText();
+    }
+
+
 }
